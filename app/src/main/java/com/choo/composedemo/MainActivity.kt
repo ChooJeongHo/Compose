79806139c632c0e2956531eaaa523a4148f8b6de
USER <USER@chujeongho.local>
2023-10-12 13:03:06 +0900

    Initial commit

---
+++ app/src/main/java/com/choo/composedemo/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.choo.composedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.choo.composedemo.ui.theme.ComposeDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeDemoTheme {
+                // 테마의 'background' 색상을 이용하는 Surface Container 를 만듭니다.
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DemoScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DemoText(message: String, fontSize: Float) {
+    Text(
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold,
+    )
+}
+
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..40f,
+        value = sliderPosition,
+        onValueChange = onPositionChange
+    )
+}
+
+@Composable
+fun DemoScreen() {
+    var sliderPosition by remember { mutableStateOf(20f) }
+
+    val handlePositionChange = { position: Float ->
+        sliderPosition = position
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        DemoText(message = "Welcome to Compose", fontSize = sliderPosition)
+
+        Spacer(modifier = Modifier.height(150.dp))
+
+        DemoSlider(
+            sliderPosition = sliderPosition,
+            onPositionChange = handlePositionChange
+        )
+
+        Text(
+            style = MaterialTheme.typography.h2,
+            text = sliderPosition.toInt().toString() + "sp"
+        )
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Preview() {
+    ComposeDemoTheme {
+        DemoScreen()
+    }
+}
